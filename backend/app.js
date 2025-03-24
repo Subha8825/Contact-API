040df8e7d072b2c80742d49b36f6f5d8c81a305d
Subha8825 <subha882585@gmail.com>
2025-03-24 12:19:38 +0530

    connectDataBase

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express=require('express')
 const app=express();
 const dotenv=require('dotenv')
-const path=require('path')
+const path=require('path');
+const connectDataBase = require('./config/connectDataBase');
 dotenv.config({path:path.join(__dirname,'config','config.env')})
+
+connectDataBase()
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server Running at port ${process.env.PORT} in ${process.env.NODE_ENV}`)
